3b2d2147f7ffdfc8cf16460d47f108a6ce617ed3
Keeney <nkeeney@oceanics.io>
2026-01-02 09:33:38 -0500

    gsi boxplot groupby simplify

--- gsi_boxplots.py
+++ gsi_boxplots.py
@@ -1,21 +1,19 @@
 """
-This looks at the GSI data to determine the time of year of spawning for gear types 
+This looks at the GSI data to determine the time of year of spawning for gear types
 and how that aligns with isotopic signatures.
 """
 
-
 from pathlib import Path
-from typing import Dict
 from enum import Enum
-from pandas import DataFrame, read_csv, to_datetime, set_option
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import subplots, savefig, subplot
-import numpy as np
+from pandas import read_csv
+from matplotlib.axes import Axes
+from matplotlib.pyplot import xticks, ylabel, legend, subplots
 from matplotlib import patches as mpatches
 
 
 figures = Path(__file__).parent / "figures"
 raw_data = Path(__file__).parent / "data" / "2023_StableIsotope_GSI_data.csv"
+
 
 class Dimension(Enum):
     """
@@ -33,143 +31,164 @@ class Dimension(Enum):
     GONAD_WEIGHT = "Gonad_Weight"
     GSI = "GSI"
 
-if __name__ == "__main__":
+
+def load_and_subset_source_data():
+    """
+    Data needed for GSI boxplots. Converts from data from, to a dictionary grouped by
+    tuples of gear and month that can be combined as needed for plotting.
+    """
     df = read_csv(
         raw_data,
         header=0,
         usecols=[
             Dimension.COLLECTION_DATE.value,
             Dimension.GEAR.value,
-            Dimension.SEX.value,
-            Dimension.SHELL_HEIGHT.value,
-            Dimension.TOTAL_VISCERA_WEIGHT.value,
-            Dimension.MUSCLE_WEIGHT.value,
-            Dimension.GONAD_WEIGHT.value,
-            Dimension.GSI.value
+            Dimension.GSI.value,
         ],
     )
+    gb = df.dropna().groupby([Dimension.GEAR.value, Dimension.COLLECTION_DATE.value])
+    return gb[Dimension.GSI.value].agg(list).to_dict()
 
-    df = df.dropna(subset=[Dimension.GSI.value, Dimension.COLLECTION_DATE.value, Dimension.GEAR.value])
 
-    # Separate by gear type
-    cage = df[df[Dimension.GEAR.value] == 'C']
-    net = df[df[Dimension.GEAR.value] == 'N']
-    farm = df[df[Dimension.GEAR.value] != 'W']
-    wild = df[df[Dimension.GEAR.value] == 'W']
+def plot_partition(
+    axis: Axes,
+    data: list[list[float]],
+    positions: list[float],
+    widths: float,
+    color: str = "black",
+):
+    """
+    Convenience function to plot a data partition as a boxplot on the given axis.
+    """
+    axis.boxplot(
+        data,
+        positions=positions,
+        widths=widths,
+        patch_artist=True,
+        boxprops={
+            "facecolor": "white",
+            "color": color,
+        },
+        medianprops={
+            "color": color,
+        },
+        whiskerprops={
+            "color": color,
+        },
+        capprops={
+            "color": color,
+        },
+        flierprops={
+            "marker": "o",
+            "markersize": 2,
+            "color": color,
+            "markerfacecolor": color,
+        },
+    )
 
-    # Separate by gear type and month
-    july_cage = cage[cage[Dimension.COLLECTION_DATE.value] == 7]
-    august_cage = cage[cage[Dimension.COLLECTION_DATE.value] == 8]
-    sept_cage = cage[cage[Dimension.COLLECTION_DATE.value] == 9]
-    oct_cage = cage[cage[Dimension.COLLECTION_DATE.value] == 10]
 
-    july_net = net[net[Dimension.COLLECTION_DATE.value] == 7]
-    august_net = net[net[Dimension.COLLECTION_DATE.value] == 8]
-    sept_net = net[net[Dimension.COLLECTION_DATE.value] == 9]
-    oct_net = net[net[Dimension.COLLECTION_DATE.value] == 10]
+def plot_farm_vs_wild_gsi(widths: float = 0.3):
+    """
+    Render boxplot figure for farmed and wild gonadosomatic index over time.
 
-    july_wild = wild[wild[Dimension.COLLECTION_DATE.value] == 7]
-    august_wild = wild[wild[Dimension.COLLECTION_DATE.value] == 8]
-    sept_wild = wild[wild[Dimension.COLLECTION_DATE.value] == 9]
-    oct_wild = wild[wild[Dimension.COLLECTION_DATE.value] == 10]
+    Farm means net or cage, but not wild.
+    """
+    groups = load_and_subset_source_data()
+    fig, ax = subplots(figsize=(6, 4))
+    positions = [1.0, 2.0, 3.0, 4.0]
+    plot_partition(
+        axis=ax,
+        data=[
+            groups.get(("C", 7), []) + groups.get(("N", 7), []),
+            groups.get(("C", 8), []) + groups.get(("N", 8), []),
+            groups.get(("C", 9), []) + groups.get(("N", 9), []),
+            groups.get(("C", 10), []) + groups.get(("N", 10), []),
+        ],
+        positions=positions,
+        widths=widths,
+        color="black",
+    )
+    plot_partition(
+        axis=ax,
+        data=[
+            groups.get(("W", 7), []),
+            groups.get(("W", 8), []),
+            groups.get(("W", 9), []),
+            groups.get(("W", 10), []),
+        ],
+        positions=[x + widths for x in positions],
+        widths=widths,
+        color="red",
+    )
+    xticks(
+        [x + widths / 2 for x in positions], ["July", "August", "September", "October"]
+    )
+    ylabel("gonadosomatic index")
+    legend(
+        handles=[
+            mpatches.Patch(color="black", label="Farm"),
+            mpatches.Patch(color="red", label="Wild"),
+        ]
+    )
+    fig.savefig(figures / "GSI_farm_vs_wild_boxplot.png")
 
-    july_farm = farm[farm[Dimension.COLLECTION_DATE.value] == 7]
-    august_farm = farm[farm[Dimension.COLLECTION_DATE.value] == 8]
-    sept_farm = farm[farm[Dimension.COLLECTION_DATE.value] == 9]
-    oct_farm = farm[farm[Dimension.COLLECTION_DATE.value] == 10]
 
-    # Boxplot design
-    # box_properties_1 = dict(facecolor = 'white', color='black', linewidth=1)
-    # median_properties_1 = dict(color='black', linewidth=1.5)
-    # whisker_properties_1 = dict(color='black')
-    # cap_properties_1 = dict(color='black', linewidth=1)
-    # flier_properties_1 = dict(marker='o', color = 'black', markerfacecolor='black', markersize=2)
+def plot_gonadosomatic_index(widths: float = 0.2):
+    """
+    Render a boxplot figure of GSI by gear type and month.
+    """
+    groups = load_and_subset_source_data()
+    fig, ax = subplots(figsize=(6, 4))
+    positions = [1.0, 2.0, 3.0, 4.0]
+    plot_partition(
+        axis=ax,
+        data=[
+            groups.get(("N", 7), []),
+            groups.get(("N", 8), []),
+            groups.get(("N", 9), []),
+            groups.get(("N", 10), []),
+        ],
+        positions=positions,
+        widths=widths,
+        color="black",
+    )
+    plot_partition(
+        axis=ax,
+        data=[
+            groups.get(("C", 7), []),
+            groups.get(("C", 8), []),
+            groups.get(("C", 9), []),
+            groups.get(("C", 10), []),
+        ],
+        positions=[x + widths for x in positions],
+        widths=widths,
+        color="blue",
+    )
+    plot_partition(
+        axis=ax,
+        data=[
+            groups.get(("W", 7), []),
+            groups.get(("W", 8), []),
+            groups.get(("W", 9), []),
+            groups.get(("W", 10), []),
+        ],
+        positions=[x + 2 * widths for x in positions],
+        widths=widths,
+        color="red",
+    )
 
-    # box_properties_2 = dict(facecolor = 'white', color='red', linewidth=1)
-    # median_properties_2 = dict(color='red', linewidth=1.5)
-    # whisker_properties_2 = dict(color='red')
-    # cap_properties_2 = dict(color='red', linewidth=1)
-    # flier_properties_2 = dict(marker='o', color = 'red', markerfacecolor='red', markersize=2)
-    
+    xticks([x + widths for x in positions], ["July", "August", "September", "October"])
+    ylabel("gonadosomatic index")
+    legend(
+        handles=[
+            mpatches.Patch(color="black", label="Net"),
+            mpatches.Patch(color="blue", label="Cage"),
+            mpatches.Patch(color="red", label="Wild"),
+        ]
+    )
+    fig.savefig(figures / "GSI_gear_boxplot.png")
 
-    # plt.boxplot([july_net[Dimension.GSI.value], august_net[Dimension.GSI.value], sept_farm[Dimension.GSI.value], oct_farm[Dimension.GSI.value]], 
-    #             positions = [1,2,3,4], 
-    #             widths = 0.3,
-    #             patch_artist=True, 
-    #             boxprops=box_properties_1, 
-    #             medianprops=median_properties_1,
-    #             whiskerprops=whisker_properties_1,
-    #             capprops=cap_properties_1,
-    #             flierprops=flier_properties_1)
-    # plt.boxplot([july_wild[Dimension.GSI.value], august_wild[Dimension.GSI.value], sept_wild[Dimension.GSI.value], oct_wild[Dimension.GSI.value]],
-    #             positions = [1.3,2.3,3.3,4.3], 
-    #             widths = 0.3,
-    #             patch_artist=True, 
-    #             boxprops=box_properties_2, 
-    #             medianprops=median_properties_2,
-    #             whiskerprops=whisker_properties_2,
-    #             capprops=cap_properties_2,
-    #             flierprops=flier_properties_2)
-    # plt.xticks([1.15,2.15,3.15,4.15], ['July', 'August', 'September', 'October'])
-    # plt.ylim(0, 35)
-    # plt.ylabel('GSI')
-    # plt.legend(handles = [
-    #         mpatches.Patch(color='black', label='Farm'),
-    #         mpatches.Patch(color='red', label='Wild'),
-    # ])
-    # plt.savefig(figures / "GSI_farm_vs_wild_boxplot.png")
 
-    box_properties = dict(facecolor = 'white', color='black', linewidth=1)
-    median_properties = dict(color='black', linewidth=1.5)
-    whisker_properties = dict(color='black')
-    cap_properties = dict(color='black', linewidth=1)
-    flier_properties = dict(marker='o', color = 'black', markerfacecolor='black', markersize=2)
-
-    box_properties_2 = dict(facecolor = 'white', color='blue', linewidth=1)
-    median_properties_2 = dict(color='blue', linewidth=1.5)
-    whisker_properties_2 = dict(color='blue')
-    cap_properties_2 = dict(color='blue', linewidth=1)
-    flier_properties_2 = dict(marker='o', color = 'blue', markerfacecolor='blue', markersize=2)
-
-    box_properties_3 = dict(facecolor = 'white', color='red', linewidth=1)
-    median_properties_3 = dict(color='red', linewidth=1.5)
-    whisker_properties_3 = dict(color='red')
-    cap_properties_3 = dict(color='red', linewidth=1)
-    flier_properties_3 = dict(marker='o', color = 'red', markerfacecolor='red', markersize=2)
-
-    plt.boxplot([july_net[Dimension.GSI.value], august_net[Dimension.GSI.value], sept_net[Dimension.GSI.value], oct_net[Dimension.GSI.value]], 
-                positions = [1,2,3,4], 
-                widths = 0.2,
-                patch_artist=True, 
-                boxprops=box_properties, 
-                medianprops=median_properties,
-                whiskerprops=whisker_properties,
-                capprops=cap_properties,
-                flierprops=flier_properties)
-    plt.boxplot([july_cage[Dimension.GSI.value], august_cage[Dimension.GSI.value], sept_cage[Dimension.GSI.value], oct_cage[Dimension.GSI.value]],
-                positions = [1.2,2.2,3.2,4.2], 
-                widths = 0.2,
-                patch_artist=True, 
-                boxprops=box_properties_2, 
-                medianprops=median_properties_2,
-                whiskerprops=whisker_properties_2,
-                capprops=cap_properties_2,
-                flierprops=flier_properties_2)
-    plt.boxplot([july_wild[Dimension.GSI.value], august_wild[Dimension.GSI.value], sept_wild[Dimension.GSI.value], oct_wild[Dimension.GSI.value]],
-                positions = [1.4,2.4,3.4,4.4], 
-                widths = 0.2,
-                patch_artist=True, 
-                boxprops=box_properties_3, 
-                medianprops=median_properties_3,
-                whiskerprops=whisker_properties_3,
-                capprops=cap_properties_3,
-                flierprops=flier_properties_3)
-    plt.xticks([1.3,2.3,3.3,4.3], ['July', 'August', 'September', 'October'])
-    plt.ylim(0, 45)
-    plt.ylabel('GSI')
-    plt.legend(handles = [
-            mpatches.Patch(color='black', label='Net'),
-            mpatches.Patch(color='blue', label='Cage'),
-            mpatches.Patch(color='red', label='Wild'),
-    ])
-    plt.savefig(figures / "GSI_gear_boxplot.png")
+if __name__ == "__main__":
+    plot_farm_vs_wild_gsi()
+    plot_gonadosomatic_index()
